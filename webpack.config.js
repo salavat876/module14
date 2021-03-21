27b7907d22101c6f2acedb8b7dea93fd82233038
salavat876 <mavlikhanov2002@mail.ru>
2021-03-22 00:48:48 +0400

    d

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const isDev = process.env.NODE_ENV === 'production';
+const isProd = !isDev;
+
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+
+const fileName = ext => isDev ?`[name].${ext}` :`[name].[contenthash].${ext}`;
 module.exports = {
     entry: './js/index.js',
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'bundle'),
         filename: 'build.js'
     },
+    plugins: [
+        new HTMLWebPackPlugin({
+            template:path.resolve(__dirname, 'index.html'),
+            filename:'index.html',
+            minify:{
+                collapseWhitespace:isProd
+            }
+        })
+    ],
     module: {
         rules: [
             {
