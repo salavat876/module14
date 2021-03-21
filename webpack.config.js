ef0554cdbea79701aad33aa1f5053eb32890d0f3
salavat876 <mavlikhanov2002@mail.ru>
2021-03-22 00:33:38 +0400

    de

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
-module.exports ={
-    
+const path = require('path');
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'bundle'),
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 }
